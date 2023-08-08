ffe805c1ec87798cae1944822d7f4122297bc89c
noel-lopez <nlopez.publi@gmail.com>
2023-08-08 19:45:01 +0200

    Added art portfolio project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,15 +10,26 @@ export interface ProjectType {
 export const projectsList: ProjectType[] = [
   {
     name: 'Wordle game',
-    description: 'Unravel word puzzles in 6 tries!\nTest your vocab skills and conquer the challenge. Addictive and fun!',
+    description:
+      'Unravel word puzzles in 6 tries!\nTest your vocab skills and conquer the challenge. Addictive and fun!',
     image: 'wordle.webp',
     codeUrl: 'https://github.com/noel-lopez/react-wordle',
     demoUrl: 'https://noel-wordle.vercel.app',
     tools: ['JSX', 'React', 'CSS', 'Vercel']
   },
   {
+    name: 'Art Gallery',
+    description:
+      'Art portfolio website that I built for my girlfriend.\nA place to showcase her art and connect with her audience.',
+    image: 'artPortfolio.webp',
+    codeUrl: 'https://github.com/noel-lopez/art-portfolio',
+    demoUrl: 'https://arumxchan-portfolio.vercel.app',
+    tools: ['Typescript', 'NextJS', 'SCSS', 'Gsap', 'Framer Motion']
+  },
+  {
     name: 'Code Toolkit @nlpz',
-    description: 'My own code utilities and two React libraries. Published on NPM and available for use.\nEmpowering developers to build faster and better!',
+    description:
+      'My own code utilities and two React libraries. Published on NPM and available for use.\nEmpowering developers to build faster and better!',
     image: 'npmLibraries.webp',
     codeUrl: 'https://github.com/noel-lopez/nlpz',
     tools: ['Typescript', 'React', 'NPM', 'Turborepo']
